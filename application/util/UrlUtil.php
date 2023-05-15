3dddf49d7cdf67ab6660e7e486a316deac476800
shinnewjean <dbwks97@gmail.com>
2023-05-15 16:51:22 +0900

    주석과추가설명 추가16_51

--- application/util/UrlUtil.php
+++ application/util/UrlUtil.php
@@ -4,16 +4,24 @@ namespace application\util;
 
 class UrlUtil {
 
-	// $_GET["url"]을 분석해서 리턴
-	public static function getUrl() {
-		return isset($_GET["url"]) ? $_GET["url"] : "" ;
-	}
+    // $_GET["url"]을 분석해서 리턴
+    public static function getUrl() {
+        // + $_GET["url"]에 값이 있으면 그 값을, 없으면 빈 문자열을 리턴
+        // + ex) localhost/user/login 이라면 user/login을 리턴
+        return $path = isset($_GET["url"]) ? $_GET["url"] : "";
+    }
 
-	// URL을 "/"로 구분해서 배열을 만들고 리턴
-	public static function getUrlArrPath() {
-		$url = UrlUtil::getUrl();
-		return $url !== "" ? explode("/", $url) : "";
-	}
+    // URL을 "/"로 구분해서 배열을 만들고 리턴
+    public static function getUrlArrPath() {
+        // static으로 선언 되어 있기 때문에, 인스턴트화 하지 않고, ::로 사용 가능
+        // $obj = new UrlUtill();
+        // $obj->getUrl();
+
+        // + UrlUtil::getUrl() 메소드를 호출하여 $url 변수에 할당
+        $url = UrlUtil::getUrl();
+        // + $url이 빈 문자열("")이 아니면 "/"를 구분자로 사용하여 문자열을 분할한 배열을, 빈 문자열이면 그대로 리턴
+        return $url !== "" ? explode("/", $url) : "";
+    }
 
 	// "/"를 "\"로 치환해주는 메소드
 	public static function replaceSlashToBackslash($str) {
--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
+// + index.php : 웹 어플리케이션의 진입점(entry point) 역할
+// + 웹 브라우저에서 요청한 URL에 따라 적절한 컨트롤러(Controller)를 호출하고
+// + 컨트롤러는 모델(Model)과 뷰(View)를 조합하여 HTML 문서를 생성하고, 웹 브라우저에 응답으로 반환함
 
-require_once("application/lib/config.php"); // config 파일
-require_once("application/lib/autoload.php"); // autoload 파일
+
+// config 파일
+// require : 해당 파일을 불러오지 못하면 페이탈 에러, 프로그램이 즉시 멈춤
+require_once("application/lib/config.php");
+require_once("application/lib/autoload.php");
 
 new application\lib\Application(); // Application 호출
+
+?>
